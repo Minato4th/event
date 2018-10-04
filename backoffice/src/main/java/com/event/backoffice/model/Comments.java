5a4d952622b06f38a27032d620b2042f81e68c86
Minato4th <borisko.aleksey@gmail.com>
2018-10-05 00:52:45 +0300

    Add LocalDateTime converter Additional data script

--- backoffice/src/main/java/com/event/backoffice/model/Comments.java
+++ backoffice/src/main/java/com/event/backoffice/model/Comments.java
@@ -22,7 +22,7 @@ public class Comments implements Serializable {
     private static final long serialVersionUID = -2786894111427327691L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "comments_id")
     private Long commentsId;
 
